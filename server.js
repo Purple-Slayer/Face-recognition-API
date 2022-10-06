1be15c6f889b0a1eef7f5a46fc03770d6160d2da
Purple-Slayer <mutlu.tonguc@outlook.com>
2022-10-06 15:31:50 +0300

    none

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", (req, res) => {res.send("it's working!")});
+
 app.post('/signin', (req, res) => {signin.handleSignin(db, bcrypt, res, req)});
 app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});    
 app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)});
