821bc881acf5c0224d6ae95c54c3654060311cd9
Purple-Slayer <mutlu.tonguc@outlook.com>
2022-10-10 21:09:38 +0300

    helpp

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res, db)});
 app.put('/image', (req, res) => {image.handleImage(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000 || 0.0.0.0;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
